e6f5814b3e13f8c6948779cd21585d72df4472fe
Ethan <edenvir99@gmail.com>
2022-12-21 19:30:55 +0900

    init

---
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.datastore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.datastore.data.UserStore
+import com.example.datastore.ui.theme.DataStoreTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DataStoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Main()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun Main() {
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val tokenValue = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val store = UserStore(context)
+    val tokenText = store.getAccessToken.collectAsState(initial = "")
+
+    Column(
+        modifier = Modifier.clickable { keyboardController?.hide() },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(text = "DataStorage Example", fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(text = tokenText.value)
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            value = tokenValue.value,
+            onValueChange = { tokenValue.value = it },
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = {
+                CoroutineScope(Dispatchers.IO).launch {
+                    store.saveToken(tokenValue.value.text)
+                }
+            }
+        ) {
+            Text(text = "Update Token")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/datastore/data/UserStore.kt
@@ -0,0 +1,27 @@
+package com.example.datastore.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserStore(private val context: Context) {
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("userToken")
+        private val USER_TOKEN_KEY = stringPreferencesKey("user_token")
+    }
+
+    val getAccessToken: Flow<String> = context.dataStore.data.map { preferences ->
+        preferences[USER_TOKEN_KEY] ?: ""
+    }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_TOKEN_KEY] = token
+        }
+    }
+}
